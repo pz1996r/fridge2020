9c55cd6e2b7e67d3312075017ba94be04f69553a
pz1996r <pz1996r@gmail.com>
2020-03-20 17:57:35 +0100

    removing folder newexpress

--- new_express/routes/logged_in.js
+++
@@ -1,9 +0,0 @@
-const auth = require('../middelwears/auth');
-const api = require('../routes.js')
-const router = api.router;
-
-router.get('/logged_in', auth, async (req, res) => {
-    res.status(200).send(JSON.stringify('Valid Token'))
-});
-
-module.exports = router;
