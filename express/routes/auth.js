a76b030bc3e57fbca08fa05e84decbbd8122cc79
pz1996r <pz1996r@gmail.com>
2020-04-10 13:05:17 +0200

    undefined token console log for the future

--- express/routes/auth.js
+++ express/routes/auth.js
@@ -2,7 +2,6 @@ const Joi = require('joi');
 const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const { User } = require('../models/user');
-// const mongoose = require('mongoose');
 const api = require('../routes.js');
 
 const { router } = api;
@@ -22,6 +21,7 @@ function validate(req) {
 }
 
 router.post('/auth', async (req, res) => {
+  console.log('test auth work');
   const { error } = validate(req.body);
   if (error) return res.status(400).send(JSON.stringify(error.details[0].message));
 
@@ -33,12 +33,10 @@ router.post('/auth', async (req, res) => {
 
   const token = await user.generateAuthToken();
   if (token === undefined) {
-    return res.status(404).send(JSON.stringify({ ...req, user }));
+    return res.status(404).send(JSON.stringify('Server Connection problem'));
   }
   console.log(user, token, req);
   return res.header('x-auth-token', token).send(_.pick(user, ['name', 'email']));
-
-  // res.send(JSON.stringify(token));
 });
 
 module.exports = router;
