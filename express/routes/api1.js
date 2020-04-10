a47fec6897118e452af75854947b76aeefcf1f76
pz1996r <pz1996r@gmail.com>
2020-04-10 20:15:50 +0200

    node mailer testing

--- express/routes/api1.js
+++ express/routes/api1.js
@@ -1,7 +1,31 @@
-const api = require('../routes.js')
-const router = api.router;
+const nodemailer = require("nodemailer");
+const api = require('../routes.js');
+
+const { router } = api;
 
 router.get('/api1', async (req, res) => {
+    async function main() {
+        const transporter = nodemailer.createTransport({
+            host: "serwer1949717.home.pl",
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'piotr@fridge.develoopers.pl',
+                pass: 'I_Om8dP_cVZEAy_pWH_qF79y2TEIcVV_c1lwHvPY'
+            }
+        });
+        const info = await transporter.sendMail({
+            from: '"piotr@fridge.develoopers.pl 👻" <piotr@fridge.develoopers.pl>', // sender address
+            to: "pz1996r@gmail.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>" // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    }
+    main().catch(console.error);
     res.json({
         'hello': 'api'
     })
