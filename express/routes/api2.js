8013bc057b4d666296fb70640397852979da646a
pz1996r <pz1996r@gmail.com>
2020-04-11 13:08:57 +0200

    repair

--- express/routes/api2.js
+++ express/routes/api2.js
@@ -1,8 +1,12 @@
 const api = require('../routes.js')
+const mongoConnection = require('../startup/db');
 
 const { router } = api;
 
 router.get('/api2', async (req, res) => {
+    console.log('API2 refreshing mongo');
+    mongoConnection();
+    console.log('API2 finish refreshing mongo');
     res.json({
         'hello': 'api2'
     })
